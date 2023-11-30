285f420685a273d4b6cd66d5122138ed4a0958d8
brosmar18 <bryangonzalez1896@icloud.com>
2023-11-29 21:17:46 -0600

    variables created. Glogal variables setup

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,15 +2,16 @@ import Navbar from '../components/navbar/Navbar';
 import Menu from '../components/menu/Menu';
 import Footer from '../components/footer/Footer';
 import { Outlet } from 'react-router-dom';
+import '../styles/globals.scss';
 
 const Layout = () => (
   <div className='page'>
     <Navbar />
     <div className='container'>
-      <div className='menu'>
+      <div className='container__menu'>
         <Menu />
       </div>
-      <div className='content'>
+      <div className='container__content'>
         <Outlet />
       </div>
     </div>
